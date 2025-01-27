d85c635f17d83f59fe5a094d9dd1934446e9d122
Aniket Vishwakarma <aniketvishwakarma2004@gmail.com>
2025-01-27 14:24:10 +0530

    bug fix

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.CORS_ORIGIN, 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
